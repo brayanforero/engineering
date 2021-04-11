a7c66dde451a7503124bcf97ae7c4c98e395244a
Brayan Forero <brayanroswll2802@gmail.com>
2021-04-11 15:16:02 -0400

    finish and add scroll reveal

--- js/main.js
+++ js/main.js
@@ -19,3 +19,31 @@ const linkAction = (e) => {
   document.getElementById("nav-menu").classList.remove("show");
 };
 navLinks.forEach((i) => i.addEventListener("click", linkAction));
+
+// SCROLL REVELA ANIMATION
+const sr = ScrollReveal({
+  origin: "top",
+  distance: "80px",
+  duration: 2000,
+  reset: false,
+});
+
+// SCROLL HOME
+sr.reveal(".home__title");
+sr.reveal(".button", { delay: 200 });
+sr.reveal(".home__img", { delay: 400 });
+sr.reveal(".home__social-icon", { interval: 200 });
+
+// SCROLL ABOUT
+
+sr.reveal(".about__img", { delay: 200 });
+sr.reveal(".about__text", { delay: 400 });
+
+// SCROLL SKILLS
+sr.reveal(".skills__subtitle", { delay: 200 });
+sr.reveal(".skills__text", { delay: 200 });
+sr.reveal(".skills__data", { interval: 200 });
+sr.reveal(".skills__img", { delay: 200 });
+
+// SCROLL WORKS
+sr.reveal(".work__img", { interval: 200 });
